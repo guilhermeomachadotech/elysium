8ed86e017c999aad3c031da450c45b44ffade619
Guilherme de Oliveira Machado <112173925+guilhermeomachadotech@users.noreply.github.com>
2024-04-04 15:48:42 -0300

    Correção de validação
    Foi corrigido o modo de como alertar o usuário que um campo não foi preenchido ou não se adequada as especificações do código em questão

--- Elysium/js/validar-form.js
+++ Elysium/js/validar-form.js
@@ -2,43 +2,60 @@ const form = document.querySelector('form');
 const nome=document.querySelector('#nome');
 const email=document.querySelector('#email');
 const msg=document.querySelector('#msg');
+const errorName = document.querySelector('.errorName');
+const errorEmail = document.querySelector('.errorEmail');
+const errorMsg = document.querySelector('.errorMsg');
 
 form.addEventListener('submit',(event)=>{
     event.preventDefault();
     
     //Verifica se o nome está vazio
     if (nome.value==='') {
-        alert("Preencha o campo nome");
+        errorName.innerHTML="<span class='errorMessage'>Preencha o campo nome!!!</span>"
+        //alert("Preencha o campo nome")
         nome.focus();
         return;
+    }else{
+        errorName.innerHTML="";
     }
     if (nome.value.length<10) {
-        alert("Nome inválido");
+        errorName.innerHTML="<span class='errorMessage'>Nome inválido</span>"
+        //alert("Nome inválido");
         nome.focus();
         return;
+    }else{
+        errorName.innerHTML="";
     }
     //Verficação de email
     
-    /*if (email.value==='') {
-        alert('Preencha o campo email');
+    if (email.value==='') {
+        errorEmail.innerHTML="<span class='errorMessage'>Preencha o campo Email!!!</span>"
         email.focus()
         return;
-    }*/
+    }else{
+        errorEmail.innerHTML='';
+    }
     if (validarEmail(email.value)) {
-        alert('Email Inválido');
+        errorEmail.innerHTML='<span class="errorMessage">Email inválido!!!</span>';
         email.focus()
         return;
+    }else{
+        errorEmail.innerHTML='';
     }
     //Vefiricação de mensagen
     if (msg.value==='') {
+        errorMsg.innerHTML="<span class='errorMessage'>Preencha o campo mensagem!!!</span>";
         msg.focus();
-        alert('Preencha o campo mensagem');
         return;
+    }else{
+        errorMsg.innerHTML="";
     }
     if (msg.value<10) {
-        alert('Mensagem inválida');
+        errorMsg.innerHTML="<span class='errorMessage'>Mensagem inválida</span>"
         msg.focus();
         return;
+    }else{
+
     }
     //Se tudo estiver certo, envie o form
     form.submit();
